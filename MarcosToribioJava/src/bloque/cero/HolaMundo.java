358ee93b8d8e25488bfa562431e5c71c8a595568
mjcortor <manucordoba95@gmail.com>
2025-02-01 12:38:21 +0100

    primer ejemplo hola mundo y sentencia IF

---
+++ MarcosToribioJava/src/bloque/cero/HolaMundo.java
@@ -0,0 +1,19 @@
+package bloque.cero;
+
+public class HolaMundo {
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+
+		
+
+		
+		//estructura de control condicional bloque if else 
+		if (1>1 && 2<3) {
+			System.out.println("verdadero");
+		} else {
+			System.out.println("falso");
+			
+		}
+
+	}
+}
