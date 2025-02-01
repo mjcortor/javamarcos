9ce771e8e881eea970cd0e1b3107a91337b232f1
mjcortor <manucordoba95@gmail.com>
2025-02-01 12:49:06 +0100

    actualizacion codigo

--- MarcosToribioJava/src/bloque/cero/HolaMundo.java
+++ MarcosToribioJava/src/bloque/cero/HolaMundo.java
@@ -3,6 +3,15 @@ package bloque.cero;
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		
+		//definicion tipos primitivos en variables.
+		byte tipoPrimitivoByte = 1;
+		short tipoPrimitivoShort = 256;
+		int tipoPrimitivoInt = 0;
+		float tipoPrimitivoFloat = 2;
+		double tipoPrimitivoDouble = 2.1;
+		char a = 'b';
+		
 
 		// estructura de control condicional bloque if else
 		if (1 > 1 && 2 < 3) {
