87bd41f3f0a31ceb6947c96d303ec83b593defa6
mjcortor <manucordoba95@gmail.com>
2025-02-01 12:50:48 +0100

    deberes

--- MarcosToribioJava/src/bloque/cero/HolaMundo.java
+++ MarcosToribioJava/src/bloque/cero/HolaMundo.java
@@ -3,15 +3,14 @@ package bloque.cero;
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
-		
-		//definicion tipos primitivos en variables.
+
+		// definicion tipos primitivos en variables.
 		byte tipoPrimitivoByte = 1;
 		short tipoPrimitivoShort = 256;
 		int tipoPrimitivoInt = 0;
 		float tipoPrimitivoFloat = 2;
 		double tipoPrimitivoDouble = 2.1;
 		char a = 'b';
-		
 
 		// estructura de control condicional bloque if else
 		if (1 > 1 && 2 < 3) {
@@ -20,6 +19,14 @@ public class HolaMundo {
 			System.out.println("falso");
 
 		}
+
+		// TODO
+
+		// probar operadores logicos y relacionales en estructuras de control if else.
+
+		// realizar un programa que indique mediante dos variables enteras si una es
+		// mayor que la otra informando de cual es mayor y cual es la menor de las que
+		// se establezcan manualmente.
 
 	}
 }
