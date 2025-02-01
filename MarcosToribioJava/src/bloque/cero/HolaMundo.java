fbecd7ef75bf242edd309b89c01200660295b813
mjcortor <manucordoba95@gmail.com>
2025-02-01 12:46:54 +0100

    actualizacion de codigo

--- MarcosToribioJava/src/bloque/cero/HolaMundo.java
+++ MarcosToribioJava/src/bloque/cero/HolaMundo.java
@@ -4,15 +4,12 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 
-		
-
-		
-		//estructura de control condicional bloque if else 
-		if (1>1 && 2<3) {
+		// estructura de control condicional bloque if else
+		if (1 > 1 && 2 < 3) {
 			System.out.println("verdadero");
 		} else {
 			System.out.println("falso");
-			
+
 		}
 
 	}
